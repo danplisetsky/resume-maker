b82af2681405094561087f54b384a1cf2100be69
Daniel Plisetsky <danplisetsky@gmail.com>
2017-11-24 15:11:53 +0200

    refactor attaching behaviors

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
 (function () {
 
-    const createColorPicker = (el, left) => {
-        let colorPicker = document.createElement('input');
-        colorPicker.setAttribute('type', 'color');
-        colorPicker.setAttribute('id', 'colorPicker');
-        colorPicker.style.left = `${left - 22}px`;
+    const forEachElemOfClass = className =>
+        action => {
+            let els = [...document.getElementsByClassName(className)];
+            els.forEach(el => action(el));
+        }
 
-        colorPicker.onchange = ev => el.style.color = ev.target.value;
+    const attachColorPickers = el => {
 
-        colorPicker.onmouseleave = ev => colorPicker.parentNode.removeChild(colorPicker);
+        const createColorPicker = (el, left) => {
+            let colorPicker = document.createElement('input');
+            colorPicker.setAttribute('type', 'color');
+            colorPicker.setAttribute('id', 'colorPicker');
+            colorPicker.style.left = `${left - 22}px`;
 
-        return colorPicker;
-    };
+            colorPicker.onchange = ev => el.style.color = ev.target.value;
 
-    const attachColorPickers = className => {
-        let els = [...document.getElementsByClassName(className)];
-        els.forEach(el => {
-            el.onmouseover = ev => el.parentNode.insertBefore(createColorPicker(el, ev.clientX), el.nextSibling);
+            colorPicker.onmouseleave = ev => colorPicker.parentNode.removeChild(colorPicker);
 
-            el.onmouseleave = ev => {
-                let colorPicker = document.getElementById('colorPicker');
-                let coord = colorPicker.getBoundingClientRect();
-                if (!(coord.left <= ev.clientX && ev.clientX <= coord.right && coord.top <= ev.clientY && ev.clientY <= coord.bottom))
-                    el.parentNode.removeChild(document.getElementById('colorPicker'));
-            }
-        });
-    };
-
-    const createInput = el => {
-        let input = document.createElement('input');
-        input.setAttribute('type', 'text');
-        input.setAttribute('value', el.innerHTML);
-
-        input.style.fontSize = getComputedStyle(el).fontSize;        
-        input.style.fontWeight = getComputedStyle(el).fontWeight;
-
-        input.onkeydown = ev => {
-            if (ev.code == 'Enter') {
-                el.innerHTML = input.value;
-                el.style.display = 'block';
-                input.parentNode.removeChild(input);
-            }
+            return colorPicker;
         };
 
-        let div = document.createElement('div');
-        div.appendChild(input);
+        el.onmouseover = ev => el.parentNode.insertBefore(createColorPicker(el, ev.clientX), el.nextSibling);
 
-        return div;
+        el.onmouseleave = ev => {
+            let colorPicker = document.getElementById('colorPicker');
+            let coord = colorPicker.getBoundingClientRect();
+            if (!(coord.left <= ev.clientX && ev.clientX <= coord.right && coord.top <= ev.clientY && ev.clientY <= coord.bottom))
+                el.parentNode.removeChild(document.getElementById('colorPicker'));
+        }
     };
 
-    const attachEditBehavior = className => {
-        let els = [...document.getElementsByClassName(className)];
-        els.forEach(el => {
-            el.ondblclick = ev => {
-                let input = createInput(el);
-                el.parentNode.insertBefore(input, el.nextSibling);
-                input.firstChild.focus();
-                el.style.display = 'none';
+    const attachEditBehavior = el => {
+        el.ondblclick = ev => {
+
+            const createInput = el => {
+                let input = document.createElement('input');
+                input.setAttribute('type', 'text');
+                input.setAttribute('value', el.innerText);
+
+                input.style.fontSize = getComputedStyle(el).fontSize;
+                input.style.fontWeight = getComputedStyle(el).fontWeight;
+
+                input.onkeydown = ev => {
+                    if (ev.code == 'Enter') {
+                        el.innerText = input.value;
+                        el.style.display = 'block';
+                        input.parentNode.removeChild(input);
+                    }
+                };
+
+                let div = document.createElement('div');
+                div.appendChild(input);
+
+                return div;
             };
-        })
+
+            let input = createInput(el);
+            el.parentNode.insertBefore(input, el.nextSibling);
+            input.firstChild.focus();
+            el.style.display = 'none';
+        }
     };
 
-    attachColorPickers('canPickColor');
-    attachEditBehavior('canEdit');
+    forEachElemOfClass('canEdit')(attachEditBehavior);
+    forEachElemOfClass('canPickColor')(attachColorPickers);
 })();
