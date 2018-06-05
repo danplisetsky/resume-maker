e7cf3e7cf35a248985292a7779e4e92562640986
Daniel Plisetsky <danplisetsky@gmail.com>
2018-06-05 11:12:16 +0300

    restyle text buttons to images

--- src/main.js
+++ src/main.js
@@ -1,15 +1,35 @@
-import attachButtonBehavior from './my/attachButtonBehavior';
-import newCV from './my/newCV';
-import saveCV from './my/saveCV';
-import loadCV from './my/loadCV';
-import templateCV from './my/templateCV';
-import print from './my/print';
+import attachButtonBehavior from "./my/attachButtonBehavior";
+import newCV from "./my/newCV";
+import saveCV from "./my/saveCV";
+import loadCV from "./my/loadCV";
+import templateCV from "./my/templateCV";
+import print from "./my/print";
 
 window.onload = () => {
-    attachButtonBehavior('newCVButton')(newCV);
-    attachButtonBehavior('saveCVButton')(saveCV);
-    attachButtonBehavior('loadCVButton')(loadCV);
-    attachButtonBehavior('templateCVButton')(templateCV);
-    attachButtonBehavior('printButton')(print);
-    newCV();
+  const buttonsAndBehaviors = [
+    {
+      id: "newCVButton",
+      callback: newCV
+    },
+    {
+      id: "saveCVButton",
+      callback: saveCV
+    },
+    {
+      id: "loadCVButton",
+      callback: loadCV
+    },
+    {
+      id: "templateCVButton",
+      callback: templateCV
+    },
+    {
+      id: "printButton",
+      callback: print
+    }
+  ];
+
+  buttonsAndBehaviors.forEach(bab => attachButtonBehavior(bab));
+
+  newCV();
 };
--- src/my/attachButtonBehavior.js
+++ src/my/attachButtonBehavior.js
@@ -1,10 +1,8 @@
-const attachButtonBehavior = id =>
-    callback => {
-        const button = document.getElementById(id);
-        if (button.tagName === 'INPUT' && button.type === 'file')
-            button.onchange = callback;
-        else
-            button.onclick = callback;
-    };
+const attachButtonBehavior = ({ id, callback }) => {
+  const button = document.getElementById(id);
+  if (button.tagName === "INPUT" && button.type === "file")
+    button.onchange = callback;
+  else button.onclick = callback;
+};
 
 export default attachButtonBehavior;
