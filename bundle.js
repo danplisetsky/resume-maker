94b596b566ecfc3b2bd18c5a41e96ff857d32942
Daniel Plisetsky <danplisetsky@gmail.com>
2018-06-28 11:54:56 +0300

    fix enter key press detection on mobile

--- bundle.js
+++ bundle.js
@@ -77,46 +77,45 @@
   };
 
   const attachEditBehavior = el => {
+    const createInput = () => {
+      const elStyle = getComputedStyle(el);
+      const prevDisplay = el.style.display;
 
-      const createInput = () => {
-          const elStyle = getComputedStyle(el);
-          const prevDisplay = el.style.display;
-
-          const input = createElement('input', {
-              type: 'text',
-              value: el.innerText,
-              style: {
-                  fontSize: elStyle.fontSize,
-                  fontWeight: elStyle.fontWeight
-              },
-              onkeydown: ev => {
-                  if (ev.code === 'Enter' || ev.code === 'NumpadEnter') {
-                      if (!input.value)
-                          alert("can't be empty!");
-                      else {
-                          el.innerText = input.value;
-                          el.style.display = prevDisplay;
-                          input.parentNode.remove();
-                          forEachElem('.actionContainer')(ac => ac.remove());
-                      }
-                  }
-              }
-          });
-
-          return createElement('div', {
-              className: 'input',
-              children: [input]
-          });
-      };
-
-      el.addEventListener('dblclick', ev => {
-          const input = createInput();
-          insertAfter(el, input);
-          input.firstChild.focus();
-          el.style.display = 'none';
-          ev.stopPropagation();
-          forEachElem('.actionContainer')(ac => ac.remove());
+      const input = createElement("input", {
+        type: "text",
+        value: el.innerText,
+        style: {
+          fontSize: elStyle.fontSize,
+          fontWeight: elStyle.fontWeight
+        },
+        onkeydown: ev => {
+          console.log(ev);
+          if (ev.key === "Enter" || ev.code === "NumpadEnter") {
+            if (!input.value) alert("can't be empty!");
+            else {
+              el.innerText = input.value;
+              el.style.display = prevDisplay;
+              input.parentNode.remove();
+              forEachElem(".actionContainer")(ac => ac.remove());
+            }
+          }
+        }
       });
+
+      return createElement("div", {
+        className: "input",
+        children: [input]
+      });
+    };
+
+    el.addEventListener("dblclick", ev => {
+      const input = createInput();
+      insertAfter(el, input);
+      input.firstChild.focus();
+      el.style.display = "none";
+      ev.stopPropagation();
+      forEachElem(".actionContainer")(ac => ac.remove());
+    });
   };
 
   const randomName = () => {
