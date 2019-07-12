8915b29a44cc89739634b1c6e041a751261659f4
Daniel Plisetsky <danplisetsky@gmail.com>
2019-07-12 14:03:10 +0300

    show popup on creating a shareable link

--- bundle.js
+++ bundle.js
@@ -1114,9 +1114,15 @@
     const clipboard = new ClipboardJS(`#${buttonId}`);
     clipboard.on("success", ev => {
       ev.clearSelection();
+      swal({
+        title: "Link created! (and copied to clipboard)",
+        icon: "success",
+        text: ev.text
+      });
     });
 
     clipboard.on("error", ev => {
+      swal("Oops!", "Something went wrong!", "error");
       console.log("something went wrong with link copying:");
       console.log(ev);
     });
